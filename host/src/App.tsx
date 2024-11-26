4f4efe047a5ffcbe18534293f3d4badfbea0c4ee
Faith <faith.morante@gmail.com>
2024-11-26 13:15:11 -0800

    initial commit

---
+++ host/src/App.tsx
@@ -0,0 +1,14 @@
+// @ts-ignore
+import React16App from "remoteApp/App";
+
+function App() {
+  return (
+    <div>
+      <h1>This is the host</h1>
+
+      <React16App />
+    </div>
+  );
+}
+
+export default App;
